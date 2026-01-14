4fa2b1b8dfdb2fcd5df0c5f92548350ca67c7d45
Barthemymickael <72488225+Barthemymickael@users.noreply.github.com>
2026-01-14 20:42:29 +0900

    Update countries.ts

--- data/countries.ts
+++ data/countries.ts
@@ -2127,7 +2127,28 @@ Je me balade un peu vers la partie ouest de ma plage, fais demi-tour en me disan
 Ce soir, ce sera burger. Je suis passé devant l’enseigne ce matin, je vois à peu près où elle se situe. Sur le chemin, mon téléphone s’éteint, plus de batterie, mais je suis dans mon quartier, donc aucun problème de localisation.
 Très bon burger et ambiance chill, avec musique californienne.`,
   mood: 'Balade côtière et contrastes'
+},
+{
+  id: 'j40',
+  day: 40,
+  title: 'Lorem ipsum dolor sit amet',
+  excerpt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.
+
+Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.
+Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
+
+Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.
+Pellentesque congue. Ut in risus volutpat libero pharetra tempor.
+
+Cras vestibulum bibendum augue. Praesent egestas leo in pede.
+Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum.`,
+  mood: 'Lorem ipsum'
 }
+
 
 
 
